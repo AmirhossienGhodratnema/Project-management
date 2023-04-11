72e9b1be01c3d13ee4b9ed47012c0c62b33d1d0f
amirhosseinghodratnema@gmail.com <amirhosseinghodratnema@gmail.com>
2023-04-11 20:06:10 +0430

    Find project

--- app/models/team.js
+++ app/models/team.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const TeamSchema = new mongoose.Schema({
     name: { type: String, require: true },
     description: { type: String },
+    userName: { type: String, require: true, uniq: true },
     users: { type: [mongoose.Types.ObjectId], default: [] },
     owner: { type: mongoose.Types.ObjectId, require: true, default: ['USER'] },
 }, { timestamps: true });
--- app/router/web/team.js
+++ app/router/web/team.js
@@ -1,3 +1,7 @@
 const route = require('express').Router();
+const { create } = require('../../controller/web/teamController');
+
+
+
 
 module.exports = { TeamRoute: route };
