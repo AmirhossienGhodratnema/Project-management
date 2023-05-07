ad3efa33592aefb32fc9fc0f1d86157056760a17
amirhosseinghodratnema@gmail.com <amirhosseinghodratnema@gmail.com>
2023-05-08 00:45:28 +0430

    Update change request

--- app/controller/web/userController.js
+++ app/controller/web/userController.js
@@ -82,6 +82,67 @@ module.exports = new class UserController {
         };
     };
 
+    async filterReq(req, res, next) {
+        try {
+            const { status } = req.params;    // Get params
+            const userID = req.user._id;    // Get my user id
+            const requistFilter = await User.aggregate([
+                { $match: { '_id': userID } },
+                {
+                    $project: {
+                        _id: 0,
+                        inviteRequest: 1,
+                        inviteRequest: {
+                            $filter: {
+                                input: '$inviteRequest',
+                                as: 'requistFilter',
+                                cond: {
+                                    $eq: ['$$requistFilter.status', status],
+                                },
+                            },
+                        },
+                    },
+                },
+            ]);
+            const requist = requistFilter?.[0]?.inviteRequest;    // Main data extract
+
+            return res.json({
+                status: 200,
+                success: true,
+                requist
+            });
+        } catch (error) {
+            next(error);
+        };
+    };
+
+
+    async changeStatusRequest(req, res, next) {
+        try {
+            const { id, status } = req.params;
+            const requist = await User.findOne({ 'inviteRequest._id': id });    // Get record request width id
+            if (!requist) throw { statusCode: 400, 'message': 'There is no such information' };    // Error request not found
+            const findRequest = requist.inviteRequest.find(item => item.id == id);    // Find requist
+            if (findRequest.status !== 'pending') throw { statusCode: 400, message: 'This request has already been rejected or accepted' };    // Error requist accepted or rejected
+            if (!['accepted', 'rejected'].includes(status)) throw { statusCode: 400, message: 'Your request is not valid' };    // Not valid reqest
+            const updateResult = await User.updateOne({ 'inviteRequest._id': id }, {
+                $set: {
+                    'inviteRequest.$.status': status,
+                }
+            });
+            console.log(updateResult)
+
+            if (updateResult.modifiedCount == 0) throw { statusCode: 400, message: 'Changes were not saved' };
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                message: 'Request update'
+            });
+        } catch (error) {
+            next(error);
+        };
+    };
+
     addSkills() { };
     editEkills() { };
     acceotInviteTeam() { };
--- app/router/web/user.js
+++ app/router/web/user.js
@@ -1,5 +1,5 @@
 const route = require('express').Router();
-const { getProfile, editProfile, uploadProfile, getAllRequest } = require('../../controller/web/userController');
+const { getProfile, editProfile, uploadProfile, getAllRequest, filterReq, changeStatusRequest } = require('../../controller/web/userController');
 
 // My modules
 const { upload_multer } = require('../../modules/multer');
@@ -17,6 +17,8 @@ route.post('/profile', getProfile);
 route.post('/editProfile', editProfile);
 route.post('/profileUpload', upload_multer.single('image'), userValidation(), uploadProfile);
 route.get('/getAllRequest', getAllRequest);
+route.post('/filterReq/:status', filterReq);
+route.post('/changeStatus/:id/:status', changeStatusRequest);
 
 
 module.exports = { UserRoute: route };
